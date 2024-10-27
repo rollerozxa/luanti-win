1add46ffd0166516993434a8ef44972a5819938e
ROllerozxa <rollerozxa@voxelmanip.se>
2024-10-27 20:54:40 +0100

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -eu
+
+apply_patch() {
+	patch -Np1 -i $1
+}
+
+# Build Luanti
+pushd luanti
+
+sudo ./util/buildbot/download_toolchain.sh /usr
+
+apply_patch ../001-silence-dirty.patch
+apply_patch ../002-run-in-place-0.patch
+apply_patch ../003-move-binary-up.patch
+apply_patch ../004-dont-bundle-unnecessary-dlls.patch
+
+EXISTING_MINETEST_DIR=$PWD \
+  ./util/buildbot/buildwin$1.sh ../B
